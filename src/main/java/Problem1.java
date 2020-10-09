45519e5b2819b5c138c11790e0a66cbc2eed3e5e
Eimi <eeimikoga@gmail.com>
2020-10-08 21:20:39 -0700

    Homework submission.

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,5 +1,5 @@
 public class Problem1 {
-    //submission 2
+    //homework submission
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         int answer = -1;
