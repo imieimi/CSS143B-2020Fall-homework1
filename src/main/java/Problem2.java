f0d6caface087387294987b8b66a23ec842ed382
Eimi <eeimikoga@gmail.com>
2020-10-08 22:46:36 -0700

    Fixed submission.

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -3,18 +3,19 @@ public class Problem2 {
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
         // Add your solution
-        int target;
-        int targetTwo;
-        for(int i = 0; i < data.length; i++){
-            target = data[i];
-            for(i = 0; i < data.length; i++){
-                targetTwo = data[i+1];
-                if(target > targetTwo){
-                    data[target] = data[targetTwo];
-                    data[targetTwo] = data[target];
+        int one = data.length;
+        int two = data.length - 1;
+            for(int i = 0; i < two; i++){
+                for(int a = 0; a < one - i - 1; a++){
+                    if(data[a] > data[a+1]){
+                        int swap = data[a];
+                        data[a] = data[a+1];
+                        data[a+1] = swap;
+                    }
                 }
-
+            }
+            for(int i = 0; i < one; i++){
+                System.out.print(data[i] + " ");
             }
         }
     }
-}
