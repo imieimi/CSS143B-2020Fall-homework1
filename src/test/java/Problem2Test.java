f29497280026844f1e33fd556d2d6131ebe85f28
Eimi <eeimikoga@gmail.com>
2020-10-16 18:10:29 -0700

    update 10/16 - 2

--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -8,9 +8,17 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
+        //This was a placeholder
+        //assertTrue(false);
         // add your tests here
-        int[] input = {4, 5, 6, 7, 8};
-        Problem2.bubbleSort(input);
+        int[] testOne = {4, 5, 6};
+        Problem2.bubbleSort(testOne);
+        System.out.println();
+        int[] testTwo = {5, 7, 2, 4, 9};
+        Problem2.bubbleSort(testTwo);
+        System.out.println();
+        int[] testThree = {3, 4, 3, 3, 2, 6};
+        Problem2.bubbleSort(testThree);
+
     }
 }
